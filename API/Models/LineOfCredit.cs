64f214d3fb25f4f81b1fcedf0a6dc3d219839315
Nicolas Dawson <nicolas.dawson@vivint.com>
2025-02-24 09:50:21 -0300

    changes for week one

---
+++ API/Models/LineOfCredit.cs
@@ -0,0 +1,10 @@
+namespace API.Models;
+
+public class LineOfCredit
+{
+    private decimal _Balance;
+    
+    public string Provider { get; set; }
+    
+    // TODO: use encapsulation to manipulate the balance of the line of credit 
+}
