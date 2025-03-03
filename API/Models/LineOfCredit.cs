2dfa4efd134d3dba65b2186905aa42b389ea39a8
CometArao <cometarao@gmail.com>
2025-03-03 08:24:12 -0300

    FEAT: modificacion LineOfCredit para usar constructor y metodos especializados

--- API/Models/LineOfCredit.cs
+++ API/Models/LineOfCredit.cs
@@ -5,6 +5,44 @@ public class LineOfCredit
     private decimal _Balance;
     
     public string Provider { get; set; }
-    
-    // TODO: use encapsulation to manipulate the balance of the line of credit 
+
+    public LineOfCredit(string provider, decimal balance)
+    {
+        if(balance < 0)
+        {
+            throw new ArgumentException("Balance must be positive");
+        }
+
+        this.Provider = provider;
+        this._Balance = balance;
+    }
+
+    public decimal GetBalance()
+    {
+
+        return this._Balance;
+    }
+
+    public void AddBalance(decimal amount)
+    {
+        if(amount < 0)
+        {
+            throw new ArgumentException("Amount must be positive");
+        }
+
+        this._Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Amount must be positive");
+        }
+        if (amount > this._Balance)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be less than the balance");
+        }
+        this._Balance -= amount;
+    }
 }
