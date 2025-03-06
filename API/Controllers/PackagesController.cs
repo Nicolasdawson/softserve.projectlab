8822cd4898ea42e97c48ccd775cd1f837d339666
Helium <kristepher.pienaar@virginiogomez.cl>
2025-03-06 19:16:51 -0300

    This is my tasks for week 1. I also added Docstrings as I find them very helpful.

--- API/Controllers/PackagesController.cs
+++ API/Controllers/PackagesController.cs
@@ -1,36 +1,75 @@
 using API.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+/// <summary>
+/// API controller for managing packages.
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class PackagesController : ControllerBase
 {
-    [HttpPost]
-    public async Task<ActionResult<Package>> CreatePackage([FromBody] Package package)
+    private readonly IPackageService _packageService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PackagesController"/> class.
+    /// </summary>
+    /// <param name="packageService">The package service.</param>
+    public PackagesController(IPackageService packageService)
     {
-        return Ok(package);
+        _packageService = packageService;
     }
-    
+
+    /// <summary>
+    /// Creates a new package.
+    /// </summary>
+    /// <param name="package">The package to create.</param>
+    /// <returns>The created package.</returns>
+    [HttpPost]
+    public async Task<ActionResult<API.Models.Package>> CreatePackage([FromBody] API.Models.Package package)
+    {
+        var result = await _packageService.CreatePackageAsync(package);
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Adds an item to a package.
+    /// </summary>
+    /// <param name="packageId">The ID of the package.</param>
+    /// <param name="itemId">The ID of the item to add.</param>
+    /// <returns>The updated package.</returns>
     [HttpPost("{packageId}/Items/{itemId}")]
     public async Task<ActionResult<Package>> AddItem([FromRoute] string packageId, [FromRoute] string itemId)
     {
-        return Ok(new Package());
+        var result = await _packageService.AddItemAsync(packageId, itemId);
+        return Ok(result);
     }
-    
-    [HttpDelete("{packageId}/Items")]
+
+    /// <summary>
+    /// Deletes an item from a package.
+    /// </summary>
+    /// <param name="packageId">The ID of the package.</param>
+    /// <param name="itemId">The ID of the item to delete.</param>
+    /// <returns>The updated package.</returns>
+    [HttpDelete("{packageId}/Items/{itemId}")]
     public async Task<ActionResult<Package>> DeleteItem([FromRoute] string packageId, [FromRoute] string itemId)
     {
-        return Ok(new Package());
+        var result = await _packageService.DeleteItemAsync(packageId, itemId);
+        return Ok(result);
     }
-    
+
+    /// <summary>
+    /// Adds a customer to a package.
+    /// </summary>
+    /// <param name="packageId">The ID of the package.</param>
+    /// <param name="customer">The customer to add.</param>
+    /// <returns>The updated package.</returns>
     [HttpPost("{packageId}/Customers")]
     public async Task<ActionResult<Package>> AddCustomer([FromRoute] string packageId, [FromBody] Customer customer)
     {
-        return Ok(new Package());
+        var result = await _packageService.AddCustomerAsync(packageId, customer);
+        return Ok(result);
     }
-    
-    //TODO: we need a domain class to process this requests, use dependency injection to use those classes 
 }
