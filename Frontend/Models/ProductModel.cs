9939ecd11f94c4c5a7f82ce5914a899ed0efa93b
Matías Pérez <matiaspereztoro2705@gmail.com>
2025-03-25 21:53:32 -0300

    Se agrgo el paquete Microsoft.Extensions.Http, se agrego HTTPClient a Program.cs para hacer llamadas a la API

---
+++ Frontend/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Frontend.Models;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public Guid CategoryId { get; set; }
+    public string Description { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+public record GetProductsResponse(IEnumerable<ProductModel> Products);
--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
 using MudBlazor.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 
 
@@ -15,5 +16,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //Register Services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddHttpClient();
 
 await builder.Build().RunAsync();
