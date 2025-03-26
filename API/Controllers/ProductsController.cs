e7222032499db578a3ef81aa612ffd1816699658
Jesus Emmanuel Hernandez Sinsel <sinselemmanuel@gmail.com>
2025-03-26 15:29:49 -0700

    implementacion de entity framework

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost("Get/{type}")]
+        public IEnumerable<string> GetProduct(string type, [FromBody] string? value)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{type}/{id}")]
+        public string GetProduct(string type, int id)
+        {
+            return "value";
+        }
+
+        [HttpPost("{type}")]
+        public void RegisterProduct(string type, [FromBody] string value)
+        {
+
+        }
+
+        [HttpPut("{type}/{id}")]
+        public void UpdateProduct(string type, int id, [FromBody] string value)
+        {
+
+        }
+
+        [HttpDelete("{type}/{id}")]
+        public void DeleteProduct(string type, int id)
+        {
+
+        }
+
+    }
+}
---
+++ API/implementations/Domain/ProductDomain.cs
@@ -0,0 +1,89 @@
+﻿using API.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.implementations.Domain
+{
+    public class ProductDomain : Controller
+    {
+        private readonly AppDbContext _db;
+        public ProductDomain(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductDomain/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductDomain/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductDomain/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDomain/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDomain/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDomain/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDomain/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
