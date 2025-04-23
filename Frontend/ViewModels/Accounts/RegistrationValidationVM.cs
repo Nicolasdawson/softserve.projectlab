cfdbafda85c7a254d1013c309d58a2e779d43433
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-22 23:06:15 -0600

    MudBlazor Register Form first advance

---
+++ Frontend/ViewModels/Accounts/RegistrationValidationVM.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace Frontend.ViewModels.Accounts
+{
+    public class RegistrationValidationVM : AbstractValidator<RegistrationVM>
+    {
+        public RegistrationValidationVM()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.");
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty()
+                .WithMessage("Confirm Password is required.")
+                .Equal(_ => _.Password)
+                .WithMessage("Passwords do not match.");
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<RegistrationVM>.CreateWithOptions((RegistrationVM)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
