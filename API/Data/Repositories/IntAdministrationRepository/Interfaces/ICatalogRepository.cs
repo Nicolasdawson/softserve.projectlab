29dff1a87ee0da4bfe2053beef19049c284bfcd4
CometArao <cometarao@gmail.com>
2025-05-08 07:46:47 -0400

    FEAT: Implementation of Repository, for the moment just Catalog and User is completed

---
+++ API/Data/Repositories/IntAdministrationRepository/Interfaces/ICatalogRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Data.Repositories.IntAdministrationRepository.Interfaces;
+
+public interface ICatalogRepository
+{
+    Task<CatalogEntity> GetByIdAsync(int catalogId);
+    Task<List<CatalogEntity>> GetAllAsync();
+    Task<CatalogEntity> AddAsync(CatalogEntity entity);
+    Task<CatalogEntity> UpdateAsync(CatalogEntity entity);
+    Task DeleteAsync(int catalogId);
+    Task<bool> ExistsAsync(int catalogId);
+
+    // Métodos para la tabla pivote
+    Task<List<CatalogCategoryEntity>> GetCatalogCategoriesAsync(int catalogId);
+    Task AddCatalogCategoryAsync(CatalogCategoryEntity pivot);
+    Task RemoveCatalogCategoryAsync(CatalogCategoryEntity pivot);
+}
